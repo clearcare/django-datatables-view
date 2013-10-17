2f10a8715e49c4fed913afa0bbb3d0eacdbbae61
Derrick Petzold <contact@derrickpetzold.com>
2013-10-17 10:01:28 -0700

    ...

--- django_datatables_view/mixins.py
+++ django_datatables_view/mixins.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.utils.encoding import force_unicode
 from django.utils.functional import Promise
-from django.utils.translation import ugettext as _
 from django.utils.cache import add_never_cache_headers
 from django.views.generic.base import TemplateView
 
@@ -44,29 +43,14 @@ class JSONResponseMixin(object):
         self.request = request
         response = None
 
-        try:
-            func_val = self.get_context_data(**kwargs)
-            if not self.is_clean:
-                assert isinstance(func_val, dict)
-                response = dict(func_val)
-                if 'result' not in response:
-                    response['result'] = 'ok'
-            else:
-                response = func_val
-        except KeyboardInterrupt:
-            # Allow keyboard interrupts through for debugging.
-            raise
-        except Exception as e:
-            logger.error('JSON view error: %s' % request.path, exc_info=True)
-
-            # Come what may, we're returning JSON.
-            if hasattr(e, 'message'):
-                msg = e.message
-                msg += str(e)
-            else:
-                msg = _('Internal error') + ': ' + str(e)
-            response = {'result': 'error',
-                        'text': msg}
+        func_val = self.get_context_data(**kwargs)
+        if not self.is_clean:
+            assert isinstance(func_val, dict)
+            response = dict(func_val)
+            if 'result' not in response:
+                response['result'] = 'ok'
+        else:
+            response = func_val
 
         json = simplejson.dumps(response, cls=LazyEncoder)
         return self.render_to_response(json)
